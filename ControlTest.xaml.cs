7385ca019ef5808ea7435e64ad2fed4292eeb481
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-30 10:21:49 -0700

    Added test messages

---
+++ ControlTest.xaml.cs
@@ -0,0 +1,9 @@
+namespace Desmond_Control_Basics;
+
+public partial class ControlTest : ContentPage
+{
+	public ControlTest()
+	{
+		InitializeComponent();
+	}
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,16 +9,27 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void btnDisplayName_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string name = entName.Text;
+            await DisplayAlert("Your Name", name, "Nice to meet you!");
         }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
+
+        //private async void btnSecondPage_Clicked(object sender, EventArgs e)
+        //{
+        //    await DisplayAlert("Alert", "You have been alerted", "OK");
+        //}
     }
 }
